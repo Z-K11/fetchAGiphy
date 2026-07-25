2882b4652bae7e87f4bf51484a642e3c28a01103
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-25 21:46:17 +0500

    load random cat gifts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const img = document.querySelector('img');
 const gifLoader = document.querySelector('#gifLoader');
 gifLoader.addEventListener('click', () => {
   fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key=hFLAx6aQY4wb7X95uoSAOJOdmOHihn6s&s=cats'
+    'https://api.giphy.com/v1/gifs/random?api_key=hFLAx6aQY4wb7X95uoSAOJOdmOHihn6s&tag=cats'
   )
     .then((response) => {
       return response.json();
