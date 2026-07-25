156125a927f203a92d7f3f6e9ab48e9e7a04b5fa
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-25 21:31:32 +0500

    add button to load random cat gif from a giphy upon click

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import './style.css';
 const img = document.querySelector('img');
-fetch(
-  'https://api.giphy.com/v1/gifs/translate?api_key=hFLAx6aQY4wb7X95uoSAOJOdmOHihn6s&s=cats'
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    img.src = response.data.images.original.url;
-  });
+const gifLoader = document.querySelector('#gifLoader');
+gifLoader.addEventListener('click', () => {
+  fetch(
+    'https://api.giphy.com/v1/gifs/translate?api_key=hFLAx6aQY4wb7X95uoSAOJOdmOHihn6s&s=cats'
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      img.src = response.data.images.original.url;
+    });
+});
